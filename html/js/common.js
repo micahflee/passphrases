04f40ea6de881ed8a4d0132fcfb967cf5f187dd3
Micah Lee <micah@micahflee.com>
2015-03-09 17:57:16 -0700

    added about tab, made links open in external browser, tweaked css

--- html/js/common.js
+++ html/js/common.js
@@ -1,1 +1,14 @@
 Passphrases = {};
+
+$(function(){
+  var gui = require('nw.gui');
+
+  // make all links open in external browser
+  $('a').each(function(){
+    var url = $(this).attr('href');
+    $(this).click(function(){
+      gui.Shell.openExternal(url);
+      return false;
+    });
+  });
+});
