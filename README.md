0eb79dcaa990c0c4f82244897e872b95758fbac7
Micah Lee <micah@micahflee.com>
2015-03-11 19:11:01 -0700

    actually restructuring project for node-webkit-builder, without grunt

--- README.md
+++ README.md
@@ -4,4 +4,12 @@ A tool to generate and help memorize cryptographically secure passphrases.
 
 ## Build Instructions
 
-Install node.js and npm. 
+Install [node.js](https://nodejs.org/) and [npm](https://www.npmjs.com/) for your operating system.
+
+Grab a copy of the source code and install the dependencies:
+
+```sh
+git clone https://github.com/micahflee/passphrases.git
+cd passphrases
+npm install
+```
---
+++ build.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+/* Passphrases | https://github.com/micahflee/passphrases
+   Copyright (C) 2015 Micah Lee <micah@micahflee.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+var NwBuilder = require('node-webkit-builder');
+var nw = new NwBuilder({
+    files: './src/**', // use the glob format
+    platforms: ['osx32', 'osx64', 'win32', 'win64', 'linux32', 'linux64']
+});
+
+nw.on('log',  console.log);
+
+nw.build().then(function () {
+   console.log('all done!');
+}).catch(function (error) {
+    console.error(error);
+});
